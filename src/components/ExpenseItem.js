1ff88dc9c9fe249ca518add18d0ee7917ef1ca3a
ShivpujanKr-NITJsr <sahil.8228053857@gmail.com>
2023-10-19 20:04:03 +0530

    added props that is properties of each object and created function to handle props for each object and shown to screen

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,23 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate=new Date(2022,3,27)
-    const expenseTitle= 'Car Insurance'
-    const expenseAmount=294.66;
-    const LocationOfExpenditure='noida'
+function ExpenseItem(props) {
+    const month=props.date.toLocaleString('en-US',{month:'long'});
+    const day=props.date.toLocaleString('en-US',{day:'2-digit'});
+    const year=props.date.getFullYear();
+
 
     return <div className='expense-item'>
-        <div>{expenseDate.toISOString()}</div>
-        <div className='expense-item__description'>
-            <h2>{expenseTitle}</h2>
-            <div className='expense-item__price'>{expenseAmount}</div>
-            <div>{LocationOfExpenditure}</div>
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
         </div>
+        <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>{props.amount}</div>
+            
+        </div>
+        <div>{props.LocationOfExpenditure}</div>
     </div>
 }
 
