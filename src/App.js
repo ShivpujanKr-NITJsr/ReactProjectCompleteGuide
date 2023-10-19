fb8eb6f39c6b03b18a39bc92e5c56bab98deaa08
ShivpujanKr-NITJsr <sahil.8228053857@gmail.com>
2023-10-20 01:46:38 +0530

    added createelment of react as well as made many changes and separated all components making like composition

--- src/App.js
+++ src/App.js
@@ -1,52 +1,43 @@
-import ExpenseItem from "./components/ExpenseItem";
-function App() {
+import React from 'react';
+import Expenses from './components/Expenses';
 
+function App() {
   const expenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      loe:'delhi',
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) ,loe:'jamua',},
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      loe:'banglore',
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
-      loe:'noida',
     },
   ];
 
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h2',{},'let\'s get started'),
+    React.createElement(Expenses,{items:expenses})
 
-  return (
-    <div>
-
-      <h2>Let's get started</h2>
-
-      
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} LocationOfExpenditure={expenses[0].loe}/>
-     
-      
-
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} LocationOfExpenditure={expenses[1].loe}/>
-
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} LocationOfExpenditure={expenses[2].loe}/>
-
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} LocationOfExpenditure={expenses[3].loe}/>
-    </div>
-  );
+  )
+  
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <Expenses items={expenses} />
+  //   </div>
+  // );
 }
 
 export default App;
-
-
-// {/* <ExpenseItem title={expenses[i].title} amount={expenses[i].amount} date={expenses[i].date} LocationOfExpenditure={expenses[i].loe}></ExpenseItem> */}
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,22 @@
 
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
+import './ExpenseItem.css';
+import React from 'react'
 
 function ExpenseItem(props) {
     
 
 
-    return <div className='expense-item'>
+    return (
+    <Card className='expense-item'>
         
         <ExpenseDate date={props.date}/>
 
         <ExpenseDetails amount={props.amount} title={props.title} loe={props.loe}/>
-    </div>
+    </Card>
+    )
 }
 
 export default ExpenseItem;
