cc0efc16e10be43534ce370cb6f6e06cb2ed49db
ShivpujanKr-NITJsr <sahil.8228053857@gmail.com>
2023-10-19 16:56:24 +0530

    also added css file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,18 @@
+import './ExpenseItem.css'
 
 function ExpenseItem() {
-    return <div><h2>
-        Expense Item!
-    </h2>
-        <li>Food Rs 10</li>
+    const expenseDate=new Date(2022,3,27)
+    const expenseTitle= 'Car Insurance'
+    const expenseAmount=294.66;
+    const LocationOfExpenditure='noida'
 
-        <li>Petrol Rs 100</li>
-
-        <li>Movies Rs 200</li>
+    return <div className='expense-item'>
+        <div>{expenseDate.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__price'>{expenseAmount}</div>
+            <div>{LocationOfExpenditure}</div>
+        </div>
     </div>
 }
 
